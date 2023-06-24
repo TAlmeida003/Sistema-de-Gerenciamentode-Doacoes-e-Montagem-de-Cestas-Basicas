6a1bb46ceae81cd34c60cb51697274231cb330a6
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2023-06-24 18:51:25 -0300

    ✨ feat (Donor): criação da classe Doador com seus atributos e métodos getters.

---
+++ ArquivosPy/Donor/__init__.py
@@ -0,0 +1,30 @@
+from ArquivosPy.Donation import Donation
+
+
+class Donor:
+
+    def __init__(self, name_donor: str = "", type_of_donor: int = 0, donation: Donation = None) -> None:
+
+        self.__type_of_donor__: int = type_of_donor
+        self.__name_donor__: str = name_donor
+        self.__donation__: Donation = donation
+
+    def add_donation(self, donated_item: int = 0, quantity_of_items: int = 0) -> None:
+
+        self.__donation__ = Donation(donated_item, quantity_of_items)
+
+    def get_name_donor(self) -> str:
+
+        return self.__name_donor__
+
+    def get_type_of_donor(self) -> int:
+
+        return self.__type_of_donor__
+
+    def get_donation(self) -> Donation:
+
+        return self.__donation__
+
+    def __repr__(self) -> str:
+
+        return str(self.__dict__)
