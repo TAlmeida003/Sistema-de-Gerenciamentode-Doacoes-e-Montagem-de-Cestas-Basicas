f3b4d858d0e74e947a490c318cf797e28292247c
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2023-06-25 13:04:41 -0300

    📚 docs(ValidataUserInputInMainMenu): adição da documentação da ValidataUserInputInMainMenu

--- ArquivosPy/ValidataUserInputInMainMenu/__init__.py
+++ ArquivosPy/ValidataUserInputInMainMenu/__init__.py
@@ -1,26 +1,42 @@
 from ArquivosPy import ScreenPrints
 
 
-def exit_main_menu_option(user_choice: str = "") -> bool:
+def exit_main_menu_option(user_choice: str) -> bool:
+    """
+    Verifica se a opção selecionada no menu principal é a opção de saída.
 
+    :param user_choice: String - Opção selecionada pelo usuário.
+    :return: Boolean - true se a opção for a de saída, false caso contrário.
+    """
     OPTION_EXIT: str = "3"
 
-    if user_choice == OPTION_EXIT:
-        return True
-    else:
+    if user_choice != OPTION_EXIT:
         return False
 
+    return True
 
-def check_option_main_menu(user_choice: str = "") -> None:
 
+def check_option_main_menu(user_choice: str) -> None:
+    """
+    Verifica se a opção selecionada no menu principal é uma opção válida.
+
+    :param user_choice: String - Opção selecionada pelo usuário.
+    :raises RuntimeError: Se a opção selecionada for inválida.
+    :return: None
+    """
     set_option: set[str] = {"1", "2", "3"}
 
     if user_choice not in set_option:
         raise RuntimeError("OPÇÃO INVALIDA")
 
 
-def is_a_valid_main_menu_option(user_choice: str = "3") -> bool:
+def is_a_valid_main_menu_option(user_choice: str) -> bool:
+    """
+    Verifica se a opção selecionada no menu principal é válida.
 
+    :param user_choice: String - Opção selecionada pelo usuário.
+    :return: Boolean - True se a opção for válida, False caso contrário.
+    """
     try:
         check_option_main_menu(user_choice)
         return True
@@ -30,6 +46,11 @@ def is_a_valid_main_menu_option(user_choice: str = "3") -> bool:
 
 
 def get_main_manu_entry() -> str:
+    """
+    Obtém a entrada do usuário para o menu principal, verificando se a opção é válida.
+
+    :return: String - Opção selecionada pelo usuário.
+    """
     user_choice: str = ScreenPrints.get_display_main_menu_option()
 
     while not is_a_valid_main_menu_option(user_choice):
