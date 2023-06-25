a78595f2cec2a97d007769ce6f1a389b52e00998
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2023-06-25 13:05:18 -0300

    📚 docs(Stock): adição da documentação da Stock

--- ArquivosPy/Stock/__init__.py
+++ ArquivosPy/Stock/__init__.py
@@ -2,6 +2,11 @@ from ArquivosPy.Item import Item
 
 
 def create_dict_itens() -> dict[str, Item]:
+    """
+    Cria e retorna um dicionário com os itens e suas informações.
+
+    :return:Dict[str, Item] - Dicionário com os itens.
+    """
     SIZE_OF_LIST: int = 11
 
     dict_items: dict[str, Item] = dict()
@@ -15,7 +20,13 @@ def create_dict_itens() -> dict[str, Item]:
     return dict_items
 
 
-def get_number_basket(dict_items: dict[str, Item] = None):
+def get_number_basket(dict_items: dict[str, Item]):
+    """
+        Retorna o número de cestas básicas a serem montadas com base na quantidade de um determinado item.
+
+        :param dict_items: Dict[str, Item] -Dicionário contendo os itens do estoque.
+        :return: Integer - Número de cestas básicas.
+        """
     KAY_SUGAR: str = "AÇÚCAR"
 
     num_basket: int = dict_items[KAY_SUGAR].get_quantity_in_baskets()
@@ -27,7 +38,13 @@ def get_number_basket(dict_items: dict[str, Item] = None):
     return num_basket
 
 
-def get_quantity_in_type_person(dict_items: dict[str, Item] = None) -> tuple[int, int]:
+def get_quantity_in_type_person(dict_items: dict[str, Item]) -> tuple[int, int]:
+    """
+    Retorna a quantidade total de itens doados por tipo de pessoa (física ou jurídica).
+
+    :param dict_items: Dict[str, Item] - Dicionário contendo os itens do estoque.
+    :return: Tuple[int, int] -  Quantidade de itens doados por pessoa física e pessoa jurídica, respectivamente.
+    """
     num_person_individual: int = 0
     num_person_legal_entity: int = 0
 
@@ -38,7 +55,13 @@ def get_quantity_in_type_person(dict_items: dict[str, Item] = None) -> tuple[int
     return num_person_individual, num_person_legal_entity
 
 
-def get_baskets_with_extra_item(dict_items: dict[str, Item] = None) -> int:
+def get_baskets_with_extra_item(dict_items: dict[str, Item]) -> int:
+    """
+    Retorna o número de cestas básicas que possuem um item extra.
+
+    :param dict_items: dict[str, Item] - Dicionário contendo os itens do estoque.
+    :return: Integer - Número de cestas básicas com item extra.
+    """
     KAY_ITEM_EXTRA: str = "OUTROS"
 
     number_basket_with_extra: int = get_number_basket(dict_items)
@@ -49,7 +72,13 @@ def get_baskets_with_extra_item(dict_items: dict[str, Item] = None) -> int:
     return number_basket_with_extra
 
 
-def get_baskets_without_extra_item(dict_items: dict[str, Item] = None) -> int:
+def get_baskets_without_extra_item(dict_items: dict[str, Item]) -> int:
+    """
+    Retorna o número de cestas básicas que não possuem um item extra.
+
+    :param dict_items: Dict[str, Item] - Dicionário contendo os itens do estoque.
+    :return: Integer - Número de cestas básicas sem item extra.
+    """
     KAY_ITEM_EXTRA: str = "OUTROS"
 
     numbers_baskets_without_extra: int = 0
