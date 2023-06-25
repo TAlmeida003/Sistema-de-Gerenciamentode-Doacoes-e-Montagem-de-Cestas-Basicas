a572d1dc8ade82ca63ac606f73f9b34bf5f24ff3
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2023-06-25 13:03:41 -0300

    📚 docs(main): adição da documentação da main

--- ArquivosPy/main.py
+++ ArquivosPy/main.py
@@ -5,7 +5,11 @@ import Stock
 import Report
 
 
-def open_option(user_choice_option: str = "", dict_items_stock: dict[str, Item] = None) -> None:
+def open_option(user_choice_option: str, dict_items_stock: dict[str, Item]) -> None:
+    """
+    Procedimento para abrir a opção desejada pelo usuário.
+    :return: None
+    """
     OPTION_ONE: str = "1"
 
     if user_choice_option == OPTION_ONE:
